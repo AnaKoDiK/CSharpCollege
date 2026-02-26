ae4bb4a89bcf9ec3f50ee68138541832f80e1fad
Sergey <viking64rus@gmail.com>
2026-02-26 21:46:53 +0300

    update

--- Projects.24.3.24.4/1. AlarmClock/AlarmClock/AlarmClock/Model/AlarmClockState.cs
+++ Projects.24.3.24.4/1. AlarmClock/AlarmClock/AlarmClock/Model/AlarmClockState.cs
@@ -32,5 +32,62 @@ namespace AlarmClock.Model
         /// Срабатывание будильника зафиксировано
         /// </summary>
         public bool IsAwakeActivated { get; set; }
+
+        /// <summary>
+        /// Время повтора будильника
+        /// </summary>
+        public TimeSpan SnoozeInterval { get; set; } = TimeSpan.FromMinutes(10);
+
+        /// <summary>
+        /// Сколько был выполнен повтор будильника
+        /// </summary>
+        public int SnoozeCount { get; set; }
+
+        /// <summary>
+        /// Максимальное количество повторов будильника
+        /// </summary>
+        public int MaxSnoozeCount { get; set; } = 2;
+
+        /// <summary>
+        /// Проверка срабатывания будильника
+        /// </summary>
+        public void CheckAlarm()
+        {
+            if (!IsAlarmActive)
+                return;
+
+            DateTime now = DateTime.Now;
+
+            if (!IsAwakeActivated && SnoozeCount == 0 && now >= AlarmTime)
+            {
+                IsAwakeActivated = true;
+                return;
+            }
+
+            if (!IsAwakeActivated && SnoozeCount > 0 && SnoozeCount <= MaxSnoozeCount && now >= AlarmTime)
+            {
+                IsAwakeActivated = true;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void FinishRinging()
+        {
+            if (SnoozeCount < MaxSnoozeCount)
+            {
+                SnoozeCount++;
+                AlarmTime = DateTime.Now + SnoozeInterval;
+                IsAwakeActivated = false;
+            }
+
+            else
+            {
+                IsAlarmActive = false;
+                IsAwakeActivated = false;
+                SnoozeCount = 0;
+            }
+        }
     }
 }
